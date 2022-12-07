ca79b23f4c5b4089985cfc70730e7a16963465ff
Hendra <hendrasyailendra@yahoo.com>
2022-12-07 15:33:05 +0800

    Fix navbar scroll

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,32 @@
-import React from "react";
-import "./navbar.css"
+import React, { useState } from "react";
+import "./navbar.css";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar-main">
-            <ul>
-                <li className="nav-item">Home</li>
-                <li className="nav-item">About</li>
-                <li className="nav-item">Service</li>
-                <li className="nav-item">Career</li>
-                <li className="nav-item">Video</li>
-                <li className="nav-item">Team</li>
-                <li className="nav-item">Testimonial</li>
-                <li className="nav-item">Feedback</li>
-                <li className="nav-item">Contact</li>
-                <li className="nav-item">Subscription</li>
-            </ul>
-        </nav>
-    )
-}
+  const [navColor, setNavColor] = useState(false);
+
+  const onScroll = () => {
+    window.scrollY >= 100 ? setNavColor(true) : setNavColor(false);
+  };
+
+  window.addEventListener("scroll", onScroll);
+  return (
+    <nav
+      className={navColor ? "navbar-main navbar-scroll-main" : "navbar-main"}
+    >
+      <ul>
+        <li className="nav-item">Home</li>
+        <li className="nav-item">About</li>
+        <li className="nav-item">Service</li>
+        <li className="nav-item">Career</li>
+        <li className="nav-item">Video</li>
+        <li className="nav-item">Team</li>
+        <li className="nav-item">Testimonial</li>
+        <li className="nav-item">Feedback</li>
+        <li className="nav-item">Contact</li>
+        <li className="nav-item">Subscription</li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
